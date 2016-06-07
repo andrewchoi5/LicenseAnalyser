8f9933d2d1e45d4f107aa8388a1b5514994611c2
dylantrachsel <dylant@ca.ibm.com>
2016-06-07 13:46:13 -0400

    Update plist and add security settings for SOAP

--- LicenseAnalyser/LicenseAnalyser/SoapInterface.swift
+++ LicenseAnalyser/LicenseAnalyser/SoapInterface.swift
@@ -13,13 +13,18 @@ import SOAPEngine64
 public class SOAPInterface {
     static func VerifyCard() {
         var soap = SOAPEngine()
+        
+        soap.authorizationMethod = SOAPAuthorization.AUTH_BASIC
+        soap.username = ""
+        soap.password = ""
+        
         soap.userAgent = "SOAPEngine"
         soap.actionNamespaceSlash = true
         soap.version = SOAPVersion.VERSION_1_1
         soap.responseHeader = true
         
         soap.setValue("param-value", forKey: "param-name")
-        soap.requestURL("http://www.my-web.com/my-service.asmx",
+        soap.requestURL("https://vxdtech.com/vxdcustomerapi/CustomerService.asmx",
                         soapAction: "http://www.my-web.com/My-Method-name",
                         completeWithDictionary: { (statusCode : Int,
                             dict : [NSObject : AnyObject]!) -> Void in
