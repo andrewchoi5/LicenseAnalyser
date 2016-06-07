ad2d9694f6dd728946ef4a9373a06016060d24dc
dylantrachsel <dylant@ca.ibm.com>
2016-06-07 13:13:43 -0400

    Add SOAP framework
    Added framework for making SOAP requests

---
+++ LicenseAnalyser/LicenseAnalyser/SoapInterface.swift
@@ -0,0 +1,36 @@
+//
+//  SoapInterface.swift
+//  LicenseAnalyser
+//
+//  Created by Dylan Trachsel on 2016-06-07.
+//  Copyright © 2016 Dylan Trachsel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SOAPEngine64
+
+public class SOAPInterface {
+    static func VerifyCard() {
+        var soap = SOAPEngine()
+        soap.userAgent = "SOAPEngine"
+        soap.actionNamespaceSlash = true
+        soap.version = SOAPVersion.VERSION_1_1
+        soap.responseHeader = true
+        
+        soap.setValue("param-value", forKey: "param-name")
+        soap.requestURL("http://www.my-web.com/my-service.asmx",
+                        soapAction: "http://www.my-web.com/My-Method-name",
+                        completeWithDictionary: { (statusCode : Int,
+                            dict : [NSObject : AnyObject]!) -> Void in
+                            
+                            var result:Dictionary = dict as Dictionary
+                            NSLog("%@", result)
+                            
+        }) { (error : NSError!) -> Void in
+            
+            NSLog("%@", error)
+        }
+    }
+    
+}
