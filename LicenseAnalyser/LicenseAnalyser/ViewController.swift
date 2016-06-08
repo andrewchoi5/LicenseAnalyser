3bd4bbb454543a95322f50a1a86d07ebfe01d91e
sonaleeshah <sonaleeshah@sonalees-mbp-2.blr.can.ibm.com>
2016-06-08 11:53:16 -0400

    Implemented Scanning back of a DL

--- LicenseAnalyser/LicenseAnalyser/ViewController.swift
+++ LicenseAnalyser/LicenseAnalyser/ViewController.swift
@@ -7,8 +7,103 @@
 //
 
 import UIKit
+import MicroBlink
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PPScanDelegate {
+    
+    func coordinatorWithError(error: NSErrorPointer) -> PPCoordinator? {
+        
+        if (PPCoordinator.isScanningUnsupportedForCameraType(PPCameraType.Back, error: error)) {
+            return nil;
+        }
+
+        let settings: PPSettings = PPSettings()
+        settings.licenseSettings.licenseKey = "GJJJYNG5-ST2WF7M3-7XLLOKB2-GK3VAWUZ-I23CQORS-W5IFVGKG-WYUDUMVW-EBS4PA2R"
+        let mrtdRecognizerSettings = PPMrtdRecognizerSettings()
+
+        settings.scanSettings.addRecognizerSettings(mrtdRecognizerSettings)
+        
+        let usdlRecognizerSettings = PPUsdlRecognizerSettings()
+        
+        settings.scanSettings.addRecognizerSettings(usdlRecognizerSettings)
+        
+        let coordinator: PPCoordinator = PPCoordinator(settings: settings)
+
+        return coordinator
+    }
+    
+    @IBAction func addPhoto(sender: AnyObject) {
+        let error: NSErrorPointer = nil
+        let coordinator:PPCoordinator?=self.coordinatorWithError(error)
+        
+        /** If scanning isn't supported, present an error */
+        if coordinator == nil {
+            let messageString: String = (error.memory?.localizedDescription) ?? ""
+            UIAlertView(title: "Warning", message: messageString, delegate: nil, cancelButtonTitle: "Ok").show()
+            return
+        }
+        
+        /** Allocate and present the scanning view controller */
+        let scanningViewController: UIViewController = coordinator!.cameraViewControllerWithDelegate(self)
+        
+        /** You can use other presentation methods as well */
+        self.presentViewController(scanningViewController, animated: true, completion: nil)
+    }
+    
+    func scanningViewControllerUnauthorizedCamera(scanningViewController: UIViewController) {
+        // Add any logic which handles UI when app user doesn't allow usage of the phone's camera
+    }
+    
+    func scanningViewController(scanningViewController: UIViewController, didFindError error: NSError) {
+        // Can be ignored. See description of the method
+    }
+    
+    func scanningViewControllerDidClose(scanningViewController: UIViewController) {
+        // As scanning view controller is presented full screen and modally, dismiss it
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func scanningViewController(scanningViewController: UIViewController?, didOutputResults results: [PPRecognizerResult]) {
+        
+        let scanConroller : PPScanningViewController = scanningViewController as! PPScanningViewController
+        
+        // Here you process scanning results. Scanning results are given in the array of PPRecognizerResult objects.
+        
+        // first, pause scanning until we process all the results
+        scanConroller.pauseScanning()
+        
+        var message : String = ""
+        var title : String = ""
+        
+        // Collect data from the result
+        for result in results {
+            if(result.isKindOfClass(PPMrtdRecognizerResult)) {
+                let mrtdResult : PPMrtdRecognizerResult = result as! PPMrtdRecognizerResult
+                title="MRTD"
+                message=mrtdResult.description
+            }
+            if(result.isKindOfClass(PPUsdlRecognizerResult)) {
+                let usdlResult : PPUsdlRecognizerResult = result as! PPUsdlRecognizerResult
+                title="USDL"
+                message=usdlResult.description
+                print("RESULTS")
+                print(usdlResult.getField(kPPCustomerIdNumber))
+                print(usdlResult.getField(kPPDateOfBirth))
+                print(usdlResult.getField(kPPAddressJurisdictionCode))
+                print(usdlResult.getField(kPPJurisdictionVehicleClass))
+            }
+        }
+
+        //present the alert view with scanned results
+        let alertView = UIAlertView(title: title, message: message, delegate: self, cancelButtonTitle: "OK")
+        alertView.show()
+        //print(message)
+    }
+    
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     @IBAction func Verify(sender: AnyObject) {
         SOAPInterface.VerifyCard()
     }
