ec40f9a5622394804716e6e32372f4eee78e66d3
dylantrachsel <dylant@ca.ibm.com>
2016-06-13 15:39:28 -0400

    Added location services api calls

--- LicenseAnalyser/ConfidenceLevelViewController.swift
+++ LicenseAnalyser/ConfidenceLevelViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 import MicroBlink
+import MapKit
 
 class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSURLConnectionDelegate, NSXMLParserDelegate, PPScanDelegate {
+    
+    var latitude = String()
+    var longitude = String()
+
     var emailAddress = String()
     
     var mutableData:NSMutableData  = NSMutableData()
     var currentElementName:NSString = ""
     
-    var isLocalValid = true
-    var isVerXValid = true
+    var isLocalValid = false
+    var isVerXValid = false
     var isPhotoSelected = false
     
     var condifenceScore = String()
@@ -31,7 +36,14 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
 //        progressIndicatorView.frame = bounds
 //        progressIndicatorView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
 //    }
-
+    let locationManager = CLLocationManager()
+    
+    func locationManagerInit() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
 
     @IBAction func button1(sender: AnyObject) {
         self.performSegueWithIdentifier("loading", sender: self)
@@ -47,7 +59,7 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        locationManagerInit()
 //        self.progressIndicatorView.frame = bounds
 //        self.progressIndicatorView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
 
@@ -128,10 +140,10 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
     }
     
     func scanningViewController(scanningViewController: UIViewController?, didOutputResults results: [PPRecognizerResult]) {
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
         isPhotoSelected = true
+        
+        
+        scanningViewController?.dismissViewControllerAnimated(false, completion: nil)
         
         let scanConroller : PPScanningViewController = scanningViewController as! PPScanningViewController
          
@@ -195,6 +207,7 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
                 
                 let person = UserLicense(firstName: firstName, lastName: lastName, fullName: fullName, LicenseIdNumber: usdlResult.getField(kPPCustomerIdNumber), formattedLicense: licenseNo, DateOfBirth: DateOfBirth, ProvinceCode: province, VehicleClass: vehicleClass, expiryDate: expireDate, dateIssued: issueDate, fullAddress: fullName, emailAddress: emailAddress, streetName: streetName, city: city)
                 
+                
                  validate(person)
             }
         }
@@ -209,9 +222,12 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
         //LocalValidation(personToVerify)
         //SocialValidation(personToVerify.emailAddress, firstName: personToVerify.firstName, lastName: personToVerify.lastName)
         //AddressVerify("", StreetName: personToVerify.streetName, City: personToVerify.city, Province: personToVerify.ProvinceCode)
+        
+        VerXVerifyLicense(personToVerify)
     }
     
     func VerXVerifyLicense(LicenseToVerify: UserLicense) {
+        print("VerX called")
         
         let prodUser = "8274851"
         let username = "1648346"
@@ -229,15 +245,15 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
         let ProductCode = "pkera56g5"
         
         let soapMessage = "<?xml version='1.0' encoding='utf-8'?><soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:ver='VerXdirect.API'><soapenv:Header/><soapenv:Body><ver:SingleDL><ver:loginId>"
-            + prodUser
+            + username
             + "</ver:loginId><ver:password>"
-            + prodPassword
+            + password
             + "</ver:password><ver:tokenKey>"
-            + prodTokenKey
+            + TokenKey
             + "</ver:tokenKey><ver:version>1.0</ver:version><ver:CompanyCode>"
-            + prodCompanyCode
+            + CompanyCode
             + "</ver:CompanyCode><ver:PCode>"
-            + prodProductCode
+            + ProductCode
             + "</ver:PCode><ver:AccessUID>?</ver:AccessUID><ver:PlaceID>?</ver:PlaceID><ver:TerminalID>?</ver:TerminalID><ver:AssociateID>?</ver:AssociateID><ver:provinceCode>"
             + LicenseToVerify.ProvinceCode
             + "</ver:provinceCode><ver:drivingLicense>"
@@ -248,8 +264,8 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
             + LicenseToVerify.VehicleClass
             + "</ver:licenseClass><ver:reference>?</ver:reference></ver:SingleDL></soapenv:Body></soapenv:Envelope>"
         
-        let urlString = "https://www.dlvcheck.com/vxdcustomerapi/CustomerService.asmx"
-        //let urlString = "https://vxdtech.com/vxdcustomerapi/CustomerService.asmx"
+        //let urlString = "https://www.dlvcheck.com/vxdcustomerapi/CustomerService.asmx"
+        let urlString = "https://vxdtech.com/vxdcustomerapi/CustomerService.asmx"
         
         let url = NSURL(string: urlString)
         
@@ -272,13 +288,18 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
         let session = NSURLSession.sharedSession()
         
         let dataTask = session.dataTaskWithRequest(theRequest, completionHandler: {(data: NSData?, response: NSURLResponse?, error : NSError?) -> Void in
-            var strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            print("Body: \(strData)")
-            
-            let xmlParser = NSXMLParser(data: data!)
-            xmlParser.delegate = self
-            xmlParser.parse()
-            xmlParser.shouldResolveExternalEntities = true
+            if (data != nil) {
+                var strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                
+                let xmlParser = NSXMLParser(data: data!)
+                xmlParser.delegate = self
+                xmlParser.parse()
+                xmlParser.shouldResolveExternalEntities = true
+                
+            }
+
+            // Next step is local validation
+            self.LocalValidation(LicenseToVerify)
         })
         
         dataTask.resume()
@@ -302,6 +323,10 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
 
     // MARK - Validation
     func LocalValidation(LicenseToValidate: UserLicense) {
+        print("local validation called")
+        
+        isLocalValid = true
+        
         // License should be 15 characters long
         if (LicenseToValidate.formattedLicense.characters.count != 15) {
             // INVALID
@@ -347,16 +372,20 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
         if (currentDate.compare(expDate!) == NSComparisonResult.OrderedDescending) {
             isLocalValid = false
         }
+        
+        SocialValidation(LicenseToValidate)
     }
     
     // MARK Social Validation
-    func SocialValidation(email : String, firstName : String, lastName : String) {
+    func SocialValidation(personToVerify : UserLicense) {
+        print("called social validation")
+        
         var url : String = "https://service.socure.com/api/2.5/EmailAuthScore?socurekey=7ed058ad-d6bb-4f1a-ba4f-28ffbcf5eafc&email="
-            + email
+            + personToVerify.emailAddress
             + "&firstname="
-            + firstName
+            + personToVerify.firstName
             + "&surname="
-            + lastName
+            + personToVerify.lastName
             + "&country=CA"
         var request : NSMutableURLRequest = NSMutableURLRequest()
         request.URL = NSURL(string: url)
@@ -365,6 +394,8 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse?, data: NSData?, error: NSError?) -> Void in
             var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
             do {
+                self.AddressVerify(personToVerify)
+                
                 if (data != nil) {
                     let jsonResult: NSObject! = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers) as? NSObject
                     if (jsonResult != nil) {
@@ -387,15 +418,16 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
         })
     }
     
-    func AddressVerify(StreetNum : String, StreetName : String, City : String, Province : String) {
+    func AddressVerify(personToVerify : UserLicense) {
+        print("address verify called")
         var url : String = "http://geocoder.ca/?stno="
-            + StreetNum
+            + ""
             + "&adresst="
-            + StreetName
+            + personToVerify.streetName
             + "&city="
-            + City
+            + personToVerify.city
             + "&prov="
-            + Province
+            + personToVerify.ProvinceCode
             + "&postal="
             + "&id="
             + "&geoit=XML"
@@ -407,6 +439,8 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse?, data: NSData?, error: NSError?) -> Void in
             var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
             do {
+                print("address verify finished")
+                self.GeoLocationVerifiy()
                 if (data != nil) {
                     var strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
                 }
@@ -418,5 +452,68 @@ class ConfidenceLevelViewController: UIViewController, UITextFieldDelegate, NSUR
                 print(error)
             }
         })
+    }
+    
+    func GeoLocationVerifiy() {
+        var url : String = "http://geocoder.ca/?latt="
+            + latitude
+            + "&longt="
+            + longitude
+            + "&reverse=1"
+            + "&geoit=XML"
+        
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        request.URL = NSURL(string: url)
+        request.HTTPMethod = "GET"
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse?, data: NSData?, error: NSError?) -> Void in
+            var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+            do {
+                print("address verify finished")
+                self.finished()
+                if (data != nil) {
+                    var strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                }
+                // enhanced - 100 or 0 from postal code
+                // gov - valid or not 100 or 0
+                // social - 10 == 25, 0 == 0
+            }
+            catch {
+                print(error)
+            }
+        })
+    }
+    
+    func finished() {
+        //self.performSegueWithIdentifier("loading", sender: self)
+        //self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        super.prepareForSegue(segue, sender: sender)
+        //(segue.destinationViewController as! ConfidenceLevelViewController).emailAddress = emailLabel.text!
+    }
+    
+    // MARK : Geolocation delegates 
+    
+}
+
+extension ConfidenceLevelViewController : CLLocationManagerDelegate {
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        var locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        latitude = String(locValue.latitude)
+        longitude = String(locValue.longitude)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: \(error)")
     }
 }
