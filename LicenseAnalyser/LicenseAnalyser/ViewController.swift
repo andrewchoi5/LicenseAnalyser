25f48bbc61714ca6fafd9a92a777031f6d1dc792
thielj <thielj@ca.ibm.com>
2016-06-14 09:53:39 -0400

    took out comments/ added spinner functionality

--- LicenseAnalyser/LicenseAnalyser/ViewController.swift
+++ LicenseAnalyser/LicenseAnalyser/ViewController.swift
@@ -35,6 +35,11 @@ class ViewController: UIViewController, UITextFieldDelegate, NSURLConnectionDele
         locationManager.requestLocation()
     }
     
+    func validateEmail(candidate: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluateWithObject(candidate)
+    }
+    
     @IBAction func onCheckboxClick(sender: AnyObject) {
         if isChecked {
             checkbox.setImage(checked, forState:.Normal);
@@ -52,7 +57,7 @@ class ViewController: UIViewController, UITextFieldDelegate, NSURLConnectionDele
             
         }
         
-//        self.checkbox.currentImage = UIImage(CGImage: checked)
+
     }
     
     @IBOutlet weak var infoLabel: UILabel!
@@ -70,20 +75,12 @@ class ViewController: UIViewController, UITextFieldDelegate, NSURLConnectionDele
     
     
     
-    @IBAction func addPhoto(sender: AnyObject) {
-    }
-    
     func imagePickerController(picker: UIImagePickerController,
                                         didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         imagePicker.dismissViewControllerAnimated(true, completion: nil)
         frontPic.image = info[UIImagePickerControllerOriginalImage] as? UIImage
     }
     
-    //the photo
-//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-//        imagePicker.dismissViewControllerAnimated(true, completion: nil)
-//        frontPic.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -97,38 +94,12 @@ class ViewController: UIViewController, UITextFieldDelegate, NSURLConnectionDele
         emailLabel.layer.borderColor = UIColor(red:33/255.0, green:180/255.0, blue:208/255.0, alpha: 1.0).CGColor
         emailLabel.layer.borderWidth = 1.0
         emailLabel.layer.cornerRadius = 8
-        //emailLabel.backgroundColor = UIColor.blackColor()
+
         emailLabel.layer.masksToBounds = true
-        
-//        infoLabel.textAlignment = NSTextAlignment.Left
+
         infoLabel.text = "Please confirm your approval for access to your camera to take picture of your drivers license and yourself for identification purposes."
         infoLabel.font = UIFont(name: "Lato-Regular", size: 15)
-//        infoLabel.backgroundColor = UIColor.whiteColor()
-//        self.view.addSubview(infoLabel)
-//
-//        view.backgroundColor = UIColor.grayColor()
-        
-        // Do any additional setup after loading the view, typically from a nib.
-        
-    
-//        for family: String in UIFont.familyNames()
-//        {
-//            print("\(family)")
-//            for names: String in UIFont.fontNamesForFamilyName(family)
-//            {
-//                print("== \(names)")
-//            }
-//        }
-        
-//        checkbox.setImage(unchecked, forState:.Normal);
-//        checkbox.setImage(checked, forState:.Highlighted);
-//        if (checkbox.state == .Highlighted) {
-//            continueButton.setImage(continue_enable, forState: .Normal)
-//            
-//        }
-//        else {
-//            
-//        }
+
         
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
